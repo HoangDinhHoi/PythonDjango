24120cba53eccd42dea6805b43975f6e492ae853
HoangDinhHoi <hoanghoihust@gmail.com>
2018-08-01 11:01:46 +0700

    01/08/2018

--- booking/forms.py
+++ booking/forms.py
@@ -1,12 +1,21 @@
 # version django == 2.0.7
 from django.forms import ModelForm
 from .models import Book
+from django import forms
 # from bootstrap_datepicker.widgets import DatePicker
 # from django.contrib.admin import widgets
 
 # form for booking
 class BookForm(ModelForm):
     """docstrings"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].label = "What is your email?"
+        self.fields['name'].label = "What is your name?"
+        self.fields['date'].label = "What date do you want?"
+        self.fields['book_time'].label = "What time do you want?"
+        self.fields['menu'].label = "What type do you want?"
+        
     class Meta:
         model = Book
         fields = ['name', 'menu', 'date', 'book_time', 'email',] # new in 2.0.7
