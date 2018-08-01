d3ee507bb9c8cd95046a528dbf6e0175fbf433e7
HoangDinhHoi <hoanghoihust@gmail.com>
2018-08-01 14:58:10 +0700

    01/08/2018

--- pricemenu/views.py
+++ pricemenu/views.py
@@ -6,6 +6,14 @@ from .forms import PriceMenuForm
 # Create your views here.
 
 def pricetable(request):
+    # show list price from PriceMenu
+    price_menu_list_3 = PriceMenu.objects.all()[:3]
+    price_menu_list_remain = PriceMenu.objects.all()[3:]
+    new_price_list = [price_menu_list_remain[i: i+3]
+                      for i in range(0, len(price_menu_list_remain), 3)]
+    
+    # get price menu form
+    form_price = PriceMenuForm
     all_price = PriceMenu.objects.all().order_by('-pub_date')
     add(request)
     return render(request, 'pricemenu/pricetable.html', locals())
